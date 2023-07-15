bc7a5d70d8c7c2a27cfa878c0dd85f5c2512363c
Mark Ericksen <brainlid@gmail.com>
2023-07-15 16:43:47 -0600

    cleanup

--- lib/chains/extraction_chain.ex
+++ lib/chains/extraction_chain.ex
@@ -1,12 +1,17 @@
 defmodule Langchain.Chains.ExtractionChain do
   @doc """
-  Define an LLMChain
+  Define an LLMChain for performing data extraction from a body of text.
+
+  Provide the schema for desired information to be parsed into. It is treated as
+  though there are 0 to many instances of the data structure being described so
+  it returns information as an array.
+
+  Originally based on:
+  - https://github.com/hwchase17/langchainjs/blob/main/langchain/src/chains/openai_functions/extraction.ts#L42
   """
   use Ecto.Schema
-  import Ecto.Changeset
   require Logger
   alias Langchain.PromptTemplate
-  alias __MODULE__
   alias Langchain.Message
   alias Langchain.Chains.LLMChain
 
@@ -14,11 +19,6 @@ defmodule Langchain.Chains.ExtractionChain do
 
   Passage:
   <%= @input %>"
-
-  # TODO: Return a %LLMChain{} that is setup with the function and knows the base prompt. Also handles parsing the response.
-  # stream: false,
-
-  # TODO: Need to either store the schema definition on the generic struct (like a map of nested, custom config) or only pass it into the call that runs the process.
 
   def run(llm, schema, prompt, opts \\ []) do
     verbose = Keyword.get(opts, :verbose, false)
@@ -37,9 +37,6 @@ defmodule Langchain.Chains.ExtractionChain do
         IO.inspect(other, label: "???????")
     end
   end
-
-  # TODO: Move this to a different module?
-  # https://github.com/hwchase17/langchainjs/blob/main/langchain/src/chains/openai_functions/extraction.ts#L42
 
   @doc """
   Build the function to expose to the LLM that can be called for data
@@ -61,47 +58,6 @@ defmodule Langchain.Chains.ExtractionChain do
         },
         required: ["info"]
       }
-      # TODO: Need to test support for this. Pass in a schema definition of type, properties and required.
-      # function getExtractionFunctions(schema: FunctionParameters) {
-      #   return [
-      #     {
-      #       name: "information_extraction",
-      #       description: "Extracts the relevant information from the passage.",
-      #       parameters: {
-      #         type: "object",
-      #         properties: {
-      #           info: {
-      #             type: "array",
-      #             items: {
-      #               type: schema.type,
-      #               properties: schema.properties,
-      #               required: schema.required,
-      #             },
-      #           },
-      #         },
-      #         required: ["info"],
-      #       },
-      #     },
-      #   ];
-      # }
-      # ],
-      # required: ["info"]
     })
   end
-
-  # export function createExtractionChain(
-  #   schema: FunctionParameters,
-  #   llm: ChatOpenAI
-  # ) {
-  #   const functions = getExtractionFunctions(schema);
-  #   const prompt = PromptTemplate.fromTemplate(_EXTRACTION_TEMPLATE);
-  #   const outputParser = new JsonKeyOutputFunctionsParser({ attrName: "info" });
-  #   return new LLMChain({
-  #     llm,
-  #     prompt,
-  #     llmKwargs: { functions },
-  #     outputParser,
-  #     tags: ["openai_functions", "extraction"],
-  #   });
-  # }
 end
