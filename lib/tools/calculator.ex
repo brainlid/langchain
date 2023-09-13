ede815804ada75379584f892c15800f821190f08
Mark Ericksen <brainlid@gmail.com>
2023-09-13 16:36:21 -0600

    calculator tool docs updated

--- lib/tools/calculator.ex
+++ lib/tools/calculator.ex
@@ -2,14 +2,97 @@ defmodule Langchain.Tools.Calculator do
   @moduledoc """
   Defines a Calculator tool for performing basic math calculations.
 
-  Defines a function to expose to an LLM and provides the `execute/2` function
-  for evaluating it the function when executed by an LLM.
+  This is an example of a pre-built `Langchain.Function` that is designed and
+  configured for a specific purpose.
+
+  This defines a function to expose to an LLM and provides an implementation for
+  the `execute/2` function for evaluating when an LLM executes the function.
+
+  When using the `Calculator` tool, you will either need to:
+
+  * make repeated calls to run the chain as the tool is called and the results
+  are then made available to the LLM before it returns the final result.
+  * OR run the chain using the `while_needs_response: true` option like this:
+    `Langchain.LLMChain.run(chain, while_needs_response: true)`
+
+
+  ## Example
+
+  The following is an example that uses a prompt where math is needed. What
+  follows is the verbose log output.
+
+      {:ok, updated_chain, %Message{} = message} =
+        %{llm: ChatOpenAI.new!(%{temperature: 0}), verbose: true}
+        |> LLMChain.new!()
+        |> LLMChain.add_message(
+          Message.new_user!("Answer the following math question: What is 100 + 300 - 200?")
+        )
+        |> LLMChain.add_functions(Calculator.new!())
+        |> LLMChain.run(while_needs_response: true)
+
+  Verbose log output:
+
+      LLM: %Langchain.ChatModels.ChatOpenAI{
+        endpoint: "https://api.openai.com/v1/chat/completions",
+        model: "gpt-3.5-turbo",
+        temperature: 0.0,
+        frequency_penalty: 0.0,
+        receive_timeout: 60000,
+        n: 1,
+        stream: false
+      }
+      MESSAGES: [
+        %Langchain.Message{
+          content: "Answer the following math question: What is 100 + 300 - 200?",
+          index: nil,
+          status: :complete,
+          role: :user,
+          function_name: nil,
+          arguments: nil
+        }
+      ]
+      FUNCTIONS: [
+        %Langchain.Function{
+          name: "calculator",
+          description: "Perform basic math calculations",
+          function: #Function<0.108164323/2 in Langchain.Tools.Calculator.execute>,
+          parameters_schema: %{
+            properties: %{
+              expression: %{
+                description: "A simple mathematical expression.",
+                type: "string"
+              }
+            },
+            required: ["expression"],
+            type: "object"
+          }
+        }
+      ]
+      SINGLE MESSAGE RESPONSE: %Langchain.Message{
+        content: nil,
+        index: 0,
+        status: :complete,
+        role: :assistant,
+        function_name: "calculator",
+        arguments: %{"expression" => "100 + 300 - 200"}
+      }
+      EXECUTING FUNCTION: "calculator"
+      FUNCTION RESULT: "200"
+      SINGLE MESSAGE RESPONSE: %Langchain.Message{
+        content: "The answer to the math question \"What is 100 + 300 - 200?\" is 200.",
+        index: 0,
+        status: :complete,
+        role: :assistant,
+        function_name: nil,
+        arguments: nil
+      }
+
   """
   require Logger
   alias Langchain.Function
 
   @doc """
-  Defines the "calculator" function.
+  Define the "calculator" function. Returns a success/failure response.
   """
   @spec new() :: {:ok, Function.t()} | {:error, Ecto.Changeset.t()}
   def new() do
@@ -27,6 +110,9 @@ defmodule Langchain.Tools.Calculator do
     })
   end
 
+  @doc """
+  Define the "calculator" function. Raises an exception if function creation fails.
+  """
   @spec new!() :: Function.t() | no_return()
   def new!() do
     case new() do
