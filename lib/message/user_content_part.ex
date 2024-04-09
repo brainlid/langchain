5c005a7cf0099a78815b82044b3804b7a3c6e355
Mark Ericksen <brainlid@gmail.com>
2024-04-09 14:29:40 -0600

    MessagePart became UserContentPart

--- lib/message/user_content_part.ex
+++ lib/message/user_content_part.ex
@@ -1,11 +1,8 @@
-defmodule LangChain.Message.MessagePart do
+defmodule LangChain.Message.UserContentPart do
   @moduledoc """
-  Models a `MessagePart`. Some LLMs support combining text, images, and possibly
-  other content as part of a single user message. A `MessagePart` represents a
+  Models a `UserContentPart`. Some LLMs support combining text, images, and possibly
+  other content as part of a single user message. A `UserContentPart` represents a
   block, or part, of a message's content that is all of one type.
-
-  Likewise, some LLMs will return assistant response messages that contain both
-  text content a tool call. For
 
   ## Types
 
@@ -38,12 +35,14 @@ defmodule LangChain.Message.MessagePart do
     field :type, Ecto.Enum, values: [:text, :image_url, :image, :tool_call], default: :text
     # tool results are sent through content as well.
     field :content, :string
+    field :options, :any, virtual: true
+
+
     # tool id links the result back to a specific tool request
     field :tool_id, :string
     field :tool_type, Ecto.Enum, values: [:function], default: :function
     field :tool_name, :string
     field :tool_arguments, :any, virtual: true
-    field :options, :any, virtual: true
   end
 
   # https://cookbook.openai.com/examples/how_to_call_functions_with_chat_models
@@ -52,7 +51,7 @@ defmodule LangChain.Message.MessagePart do
   #   results = execute_function_call(assistant_message)
   #   messages.append({"role": "function", "tool_call_id": assistant_message.tool_calls[0].id, "name": assistant_message.tool_calls[0].function.name, "content": results})
 
-  @type t :: %MessagePart{}
+  @type t :: %UserContentPart{}
 
   @update_fields [:type, :content, :options]
   @create_fields @update_fields
@@ -65,7 +64,7 @@ defmodule LangChain.Message.MessagePart do
   """
   @spec new(attrs :: map()) :: {:ok, t()} | {:error, Ecto.Changeset.t()}
   def new(attrs \\ %{}) do
-    %MessagePart{}
+    %UserContentPart{}
     |> cast(attrs, @create_fields)
     |> common_validations()
     |> apply_action(:insert)
@@ -86,7 +85,7 @@ defmodule LangChain.Message.MessagePart do
   end
 
   @doc """
-  Create a new MessagePart that contains text. Raises an exception if not valid.
+  Create a new UserContentPart that contains text. Raises an exception if not valid.
   """
   @spec text!(String.t()) :: t() | no_return()
   def text!(content) do
@@ -94,7 +93,7 @@ defmodule LangChain.Message.MessagePart do
   end
 
   @doc """
-  Create a new MessagePart that contains an image encoded as base64 data. Raises
+  Create a new UserContentPart that contains an image encoded as base64 data. Raises
   an exception if not valid.
 
   ## Options
@@ -108,7 +107,7 @@ defmodule LangChain.Message.MessagePart do
   end
 
   @doc """
-  Create a new MessagePart that contains a URL to an image. Raises an exception if not valid.
+  Create a new UserContentPart that contains a URL to an image. Raises an exception if not valid.
   """
   @spec image_url!(String.t()) :: t() | no_return()
   def image_url!(content) do
@@ -116,7 +115,7 @@ defmodule LangChain.Message.MessagePart do
   end
 
   @doc """
-  Create a new MessagePart that represents a tool call.
+  Create a new UserContentPart that represents a tool call.
   """
   @spec tool_call(attrs :: map()) :: {:ok, t()} | {:error, Ecto.Changeset.t()}
   def tool_call(attrs) do
@@ -126,7 +125,7 @@ defmodule LangChain.Message.MessagePart do
   end
 
   @doc """
-  Create a new MessagePart that represents a tool call. Returns the MessagePart
+  Create a new UserContentPart that represents a tool call. Returns the UserContentPart
   or raises an exception if invalid.
   """
   @spec tool_call!(attrs :: map()) :: t() | no_return()
@@ -144,7 +143,7 @@ defmodule LangChain.Message.MessagePart do
   Changeset when setting up a tool_call.
   """
   def tool_changeset(attrs) do
-    %MessagePart{}
+    %UserContentPart{}
     |> cast(attrs, @tool_create_fields)
     |> put_change(:type, :tool_call)
     |> validate_required([:type, :tool_name])
--- test/message/user_content_part_test.exs
+++ test/message/user_content_part_test.exs
@@ -1,18 +1,18 @@
-defmodule LangChain.Message.MessagePartTest do
+defmodule LangChain.Message.UserContentPartTest do
   use ExUnit.Case
-  doctest LangChain.Message.MessagePart
-  alias LangChain.Message.MessagePart
+  doctest LangChain.Message.UserContentPart
+  alias LangChain.Message.UserContentPart
   alias LangChain.LangChainError
 
   describe "new/1" do
     test "accepts valid settings" do
-      {:ok, %MessagePart{} = part} = MessagePart.new(%{type: :text, content: "Hello!"})
+      {:ok, %UserContentPart{} = part} = UserContentPart.new(%{type: :text, content: "Hello!"})
       assert part.type == :text
       assert part.content == "Hello!"
     end
 
     test "returns error when invalid" do
-      assert {:error, changeset} = MessagePart.new(%{"type" => nil, "content" => nil})
+      assert {:error, changeset} = UserContentPart.new(%{"type" => nil, "content" => nil})
       refute changeset.valid?
       assert {"can't be blank", _} = changeset.errors[:type]
       assert {"can't be blank", _} = changeset.errors[:content]
@@ -21,49 +21,49 @@ defmodule LangChain.Message.MessagePartTest do
 
   describe "new!/1" do
     test "accepts valid settings" do
-      %MessagePart{} = part = MessagePart.new!(%{type: :text, content: "Hello!"})
+      %UserContentPart{} = part = UserContentPart.new!(%{type: :text, content: "Hello!"})
       assert part.type == :text
       assert part.content == "Hello!"
     end
 
     test "returns error when invalid" do
       assert_raise LangChain.LangChainError, "content: can't be blank", fn ->
-        MessagePart.text!(nil)
+        UserContentPart.text!(nil)
       end
     end
   end
 
   describe("text!/1") do
-    test "returns a text configured MessagePart" do
-      %MessagePart{} = part = MessagePart.text!("Hello!")
+    test "returns a text configured UserContentPart" do
+      %UserContentPart{} = part = UserContentPart.text!("Hello!")
       assert part.type == :text
       assert part.content == "Hello!"
     end
   end
 
   describe("image!/1") do
-    test "returns an image data configured MessagePart" do
-      %MessagePart{} = part = MessagePart.image!(:base64.encode("fake_image_data"))
+    test "returns an image data configured UserContentPart" do
+      %UserContentPart{} = part = UserContentPart.image!(:base64.encode("fake_image_data"))
       assert part.type == :image
       assert part.content == "ZmFrZV9pbWFnZV9kYXRh"
     end
   end
 
   describe("image_url!/1") do
-    test "returns a image_url configured MessagePart" do
+    test "returns a image_url configured UserContentPart" do
       url =
         "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg"
 
-      %MessagePart{} = part = MessagePart.image_url!(url)
+      %UserContentPart{} = part = UserContentPart.image_url!(url)
       assert part.type == :image_url
       assert part.content == url
     end
   end
 
   describe "tool_call/1" do
-    test "returns MessagePart configured for tool_call" do
-      assert {:ok, %MessagePart{} = part} =
-               MessagePart.tool_call(%{
+    test "returns UserContentPart configured for tool_call" do
+      assert {:ok, %UserContentPart{} = part} =
+               UserContentPart.tool_call(%{
                  tool_name: "greeting",
                  tool_type: :function,
                  tool_arguments: Jason.encode!(%{name: "Tom"})
@@ -78,7 +78,7 @@ defmodule LangChain.Message.MessagePartTest do
 
     test "adds error when JSON is invalid" do
       {:error, changeset} =
-        MessagePart.tool_call(%{
+        UserContentPart.tool_call(%{
           tool_name: "greeting",
           tool_type: :function,
           tool_arguments: "{\"invalid\"}"
@@ -88,16 +88,16 @@ defmodule LangChain.Message.MessagePartTest do
     end
 
     test "returns error when required values missing" do
-      {:error, changeset} = MessagePart.tool_call(%{tool_name: nil})
+      {:error, changeset} = UserContentPart.tool_call(%{tool_name: nil})
       assert {"can't be blank", _} = changeset.errors[:tool_name]
     end
   end
 
   describe "tool_call!/1" do
-    test "returns valid MessagePart" do
-      %MessagePart{} =
+    test "returns valid UserContentPart" do
+      %UserContentPart{} =
         part =
-        MessagePart.tool_call!(%{
+        UserContentPart.tool_call!(%{
           tool_name: "greeting",
           tool_type: :function,
           tool_arguments: Jason.encode!(%{name: "Tom"})
@@ -112,7 +112,7 @@ defmodule LangChain.Message.MessagePartTest do
 
     test "raises error when invalid" do
       assert_raise LangChainError, "tool_name: can't be blank", fn ->
-        MessagePart.tool_call!(%{tool_name: nil})
+        UserContentPart.tool_call!(%{tool_name: nil})
       end
     end
   end
