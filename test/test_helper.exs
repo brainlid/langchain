b2df785782110aaf9cc04f0d771d6d92014fbdf0
Tom X Nguyen <tom81094@gmail.com>
2024-09-22 18:14:46 +0700

    chore: revert env comment from testing

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,7 +1,7 @@
 # Load the ENV key for running live OpenAI tests.
 Application.put_env(:langchain, :openai_key, System.fetch_env!("OPENAI_API_KEY"))
-# Application.put_env(:langchain, :anthropic_key, System.fetch_env!("ANTHROPIC_API_KEY"))
-# Application.put_env(:langchain, :google_ai_key, System.fetch_env!("GOOGLE_API_KEY"))
+Application.put_env(:langchain, :anthropic_key, System.fetch_env!("ANTHROPIC_API_KEY"))
+Application.put_env(:langchain, :google_ai_key, System.fetch_env!("GOOGLE_API_KEY"))
 
 Mimic.copy(LangChain.ChatModels.ChatOpenAI)
 Mimic.copy(LangChain.ChatModels.ChatAnthropic)
