3e6b68176deb0fef8490d430db36fd4fa38732ac
joaquin <iglesiastj@gmail.com>
2025-02-09 12:21:37 +0100

    added more tests and ran formatter

--- lib/text_splitter.ex
+++ lib/text_splitter.ex
@@ -13,10 +13,10 @@ defmodule LangChain.TextSplitter do
   @type t :: %TextSplitter{}
 
   @update_fields [
-   :separator,
-   :chunk_size,
-   :chunk_overlap
-   ]
+    :separator,
+    :chunk_size,
+    :chunk_overlap
+  ]
   @create_fields @update_fields
 
   def new(attrs \\ %{}) do
@@ -24,7 +24,7 @@ defmodule LangChain.TextSplitter do
     |> cast(attrs, @create_fields)
     |> apply_action(:insert)
   end
-  
+
   def split_text(%TextSplitter{} = text_splitter, text) do
     text
     |> split_text_with_regex(text_splitter)
@@ -33,7 +33,8 @@ defmodule LangChain.TextSplitter do
 
   defp split_text_with_regex(
          text,
-         %TextSplitter{} = text_splitter) do
+         %TextSplitter{} = text_splitter
+       ) do
     text
     |> String.split(text_splitter.separator)
     |> Enum.filter(fn x -> x != "" end)
@@ -44,23 +45,28 @@ defmodule LangChain.TextSplitter do
   defp merge_split_helper(d, acc, text_splitter) do
     total = acc.total
     len = String.length(d)
-    if (total <= text_splitter.chunk_overlap) or
-         (total + len + String.length(text_splitter.separator)
-         <= text_splitter.chunk_size) and
-         (total <= 0) do
+
+    if total <= text_splitter.chunk_overlap or
+         (total + len + String.length(text_splitter.separator) <=
+            text_splitter.chunk_size and
+            total <= 0) do
       acc
     else
-      new_total = acc.total - (acc.current_doc
-      |> Enum.at(0, "")
-      |> String.length) - String.length(text_splitter.separator)
+      new_total =
+        acc.total -
+          (acc.current_doc
+           |> Enum.at(0, "")
+           |> String.length()) - String.length(text_splitter.separator)
 
       new_current_doc =
         acc.current_doc
         |> Enum.drop(1)
+
       merge_split_helper(
         d,
         %{acc | total: new_total, current_doc: new_current_doc},
-        text_splitter)
+        text_splitter
+      )
     end
   end
 
@@ -68,35 +74,38 @@ defmodule LangChain.TextSplitter do
     separator_len = String.length(text_splitter.separator)
     first_split = List.first(splits)
     acc = %{current_doc: [first_split], docs: [], total: String.length(first_split)}
-    output_acc = splits
-    |> Enum.drop(1)
-    |> Enum.reduce(
-      acc,
-      fn d, acc ->
-        len = String.length(d)
-        separator_length =
-          if Enum.count(acc.current_doc) > 0,
-             do: String.length(text_splitter.separator), else: 0
-        
-        acc =
-          if (acc.total + len + separator_length) >
-               text_splitter.chunk_size do
-                if Enum.count(acc.current_doc) do
-                  doc = join_docs(acc.current_doc, text_splitter.separator)
-                  acc = %{acc | docs: acc.docs ++ [doc]}
-                  merge_split_helper(d, acc, text_splitter)
-                else
-                  acc
-                end
+
+    output_acc =
+      splits
+      |> Enum.drop(1)
+      |> Enum.reduce(
+        acc,
+        fn d, acc ->
+          len = String.length(d)
+
+          separator_length =
+            if Enum.count(acc.current_doc) > 0,
+              do: String.length(text_splitter.separator),
+              else: 0
+
+          acc =
+            if acc.total + len + separator_length >
+                 text_splitter.chunk_size do
+              if Enum.count(acc.current_doc) do
+                doc = join_docs(acc.current_doc, text_splitter.separator)
+                acc = %{acc | docs: acc.docs ++ [doc]}
+                merge_split_helper(d, acc, text_splitter)
               else
                 acc
+              end
+            else
+              acc
+            end
+
+          %{acc | current_doc: acc.current_doc ++ [d], total: acc.total + separator_len + len}
         end
-        %{acc |
-          current_doc: acc.current_doc ++ [d],
-          total: acc.total + separator_len + len
-        }
-      end
-    )
+      )
+
     output_acc.docs ++ [join_docs(output_acc.current_doc, text_splitter.separator)]
   end
 end
--- test/text_splitter_test.exs
+++ test/text_splitter_test.exs
@@ -8,35 +8,83 @@ defmodule TextSplitterTest do
       expected_splitter = %TextSplitter{
         separator: " ",
         chunk_overlap: 0,
-        chunk_size: 2,
+        chunk_size: 2
       }
+
       assert {:ok, %TextSplitter{} = output_splitter} =
-        %{separator: " ", chunk_overlap: 0, chunk_size: 2,}
-        |> TextSplitter.new()
+               %{separator: " ", chunk_overlap: 0, chunk_size: 2}
+               |> TextSplitter.new()
+
       assert expected_splitter == output_splitter
     end
-    
+
     test "Splitting by character count" do
       text = "foo bar baz 123"
       expected_output = ["foo bar", "bar baz", "baz 123"]
+
       {:ok, character_splitter} =
-        TextSplitter.new(
-           %{separator: " ", chunk_size: 7, chunk_overlap: 3})
+        TextSplitter.new(%{separator: " ", chunk_size: 7, chunk_overlap: 3})
+
       output =
         character_splitter
         |> TextSplitter.split_text(text)
+
       assert expected_output == output
     end
 
     test "Splitting character by count doesn't create empty documents" do
       text = "foo  bar"
       expected_output = ["foo", "bar"]
+
       {:ok, character_splitter} =
-        TextSplitter.new(
-           %{separator: " ", chunk_size: 2, chunk_overlap: 0})
+        TextSplitter.new(%{separator: " ", chunk_size: 2, chunk_overlap: 0})
+
       output =
         character_splitter
         |> TextSplitter.split_text(text)
+
+      assert expected_output == output
+    end
+
+    test "Edge cases are separators" do
+      text = "f  b"
+      expected_output = ["f", "b"]
+
+      {:ok, character_splitter} =
+        TextSplitter.new(%{separator: " ", chunk_size: 2, chunk_overlap: 0})
+
+      output =
+        character_splitter
+        |> TextSplitter.split_text(text)
+
+      assert expected_output == output
+    end
+
+    test "Splitting by character count on long words" do
+      text = "foo bar baz a a"
+      expected_output = ["foo", "bar", "baz", "a a"]
+
+      {:ok, character_splitter} =
+        TextSplitter.new(%{separator: " ", chunk_size: 3, chunk_overlap: 1})
+
+      output =
+        character_splitter
+        |> TextSplitter.split_text(text)
+
+      assert expected_output == output
+    end
+
+    test "Splitting by character count when shorter words are first" do
+      text = "a a foo bar baz"
+      expected_output = ["a a", "foo", "bar", "baz"]
+
+      {:ok, character_splitter} =
+        TextSplitter.new(%{separator: " ", chunk_size: 3, chunk_overlap: 1})
+
+      output =
+        character_splitter
+        |> TextSplitter.split_text(text)
+
       assert expected_output == output
     end
   end
