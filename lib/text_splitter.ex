2b07ad0444ae4890866e7d00f08deb87ced3277c
joaquin <iglesiastj@gmail.com>
2025-02-09 01:17:52 +0100

    wip on text splitter

---
+++ lib/text_splitter.ex
@@ -0,0 +1,71 @@
+defmodule LangChain.TextSplitter do
+  @separator " "
+  @chunk_size 7
+  @chunk_overlap 3
+  
+  def split_text(text) do
+    splits = split_text_with_regex(text)
+    merge_splits(splits)
+  end
+
+  defp split_text_with_regex(text) do
+    String.split(text, @separator)
+  end
+
+  defp join_docs(docs), do: Enum.join(docs, @separator)
+
+  defp merge_split_helper(d, acc) do
+    IO.puts("RECURSION!!!!!!!!!!!!!!!!!!!")
+    IO.inspect(acc)
+    total = acc.total
+    len = String.length(d)
+    if (total < @chunk_overlap) or
+         (total + len + String.length(@separator) < @chunk_size) and
+         (total < 0) do
+      acc
+    else
+      new_total = acc.total - (acc.current_doc
+      |> Enum.at(0, "")
+      |> String.length) - String.length(@separator)
+
+      new_current_doc =
+        acc.current_doc
+        |> Enum.drop(1)
+      merge_split_helper(
+        d,
+        %{acc | total: new_total, current_doc: new_current_doc})
+    end
+  end
+
+  defp merge_splits(splits) do
+    separator_len = String.length(@separator)
+    first_split = List.first(splits)
+    acc = %{current_doc: [first_split], docs: [], total: String.length(first_split)}
+    splits
+    |> Enum.drop(1)
+    |> Enum.reduce(
+      acc,
+      fn d, acc ->
+        IO.puts("REDUCTION!!!!!")
+        IO.inspect(acc)
+        len = String.length(d)
+        doc = join_docs(acc.current_doc)
+        acc =
+          if (acc.total + len) > @chunk_size do
+                if Enum.count(acc.current_doc) do
+                  acc = %{acc | docs: acc.docs ++ [doc]}
+                  merge_split_helper(d, acc)
+                else
+                  acc
+                end
+              else
+                acc
+        end
+        %{acc |
+          current_doc: acc.current_doc ++ [d],
+          total: acc.total + separator_len + len
+        }
+      end
+    )
+  end
+end
---
+++ test/text_splitter_test.exs
@@ -0,0 +1,15 @@
+defmodule TextSplitterTest do
+  use ExUnit.Case
+
+  alias LangChain.TextSplitter
+
+  describe "CharacterTextSplitter" do
+    test "text splitting by character count" do
+      text = "foo bar baz 123"
+      expected_output = ["foo bar", "bar baz", "baz 123"]
+      output = TextSplitter.split_text(text)
+      assert output == expected_output
+    end
+  end
+  
+end
