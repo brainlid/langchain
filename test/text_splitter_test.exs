038ffe469833e86a740573bc6c18bb005966bfe7
joaquin <iglesiastj@gmail.com>
2025-02-09 12:03:01 +0100

    test cleanup

--- test/text_splitter_test.exs
+++ test/text_splitter_test.exs
@@ -16,7 +16,6 @@ defmodule TextSplitterTest do
       assert expected_splitter == output_splitter
     end
     
-    @tag :wip
     test "Splitting by character count" do
       text = "foo bar baz 123"
       expected_output = ["foo bar", "bar baz", "baz 123"]
@@ -29,7 +28,6 @@ defmodule TextSplitterTest do
       assert expected_output == output
     end
 
-    @tag :wip
     test "Splitting character by count doesn't create empty documents" do
       text = "foo  bar"
       expected_output = ["foo", "bar"]
