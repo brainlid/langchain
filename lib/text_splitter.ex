7e16283308e6aa76ef03e92a9761f0fc5d39ef2d
joaquin <iglesiastj@gmail.com>
2025-02-09 12:02:47 +0100

    functions use module record and are parametrizable

--- lib/text_splitter.ex
+++ lib/text_splitter.ex
@@ -1,45 +1,71 @@
 defmodule LangChain.TextSplitter do
-  @separator " "
-  @chunk_size 7
-  @chunk_overlap 3
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias __MODULE__
+
+  @primary_key false
+  embedded_schema do
+    field :separator, :string, default: " "
+    field :chunk_size, :integer
+    field :chunk_overlap, :integer
+  end
+
+  @type t :: %TextSplitter{}
+
+  @update_fields [
+   :separator,
+   :chunk_size,
+   :chunk_overlap
+   ]
+  @create_fields @update_fields
+
+  def new(attrs \\ %{}) do
+    %TextSplitter{}
+    |> cast(attrs, @create_fields)
+    |> apply_action(:insert)
+  end
   
-  def split_text(text) do
+  def split_text(%TextSplitter{} = text_splitter, text) do
     text
-    |> split_text_with_regex
-    |> merge_splits
+    |> split_text_with_regex(text_splitter)
+    |> merge_splits(text_splitter)
   end
 
-  defp split_text_with_regex(text) do
-    String.split(text, @separator)
+  defp split_text_with_regex(
+         text,
+         %TextSplitter{} = text_splitter) do
+    text
+    |> String.split(text_splitter.separator)
+    |> Enum.filter(fn x -> x != "" end)
   end
 
-  defp join_docs(docs), do: Enum.join(docs, @separator)
+  defp join_docs(docs, separator), do: Enum.join(docs, separator)
 
-  defp merge_split_helper(d, acc) do
-    IO.puts("RECURSION!!!!!!!!!!!!!!!!!!!")
-    IO.inspect(acc)
+  defp merge_split_helper(d, acc, text_splitter) do
     total = acc.total
     len = String.length(d)
-    if (total <= @chunk_overlap) or
-         (total + len + String.length(@separator) <= @chunk_size) and
+    if (total <= text_splitter.chunk_overlap) or
+         (total + len + String.length(text_splitter.separator)
+         <= text_splitter.chunk_size) and
          (total <= 0) do
       acc
     else
       new_total = acc.total - (acc.current_doc
       |> Enum.at(0, "")
-      |> String.length) - String.length(@separator)
+      |> String.length) - String.length(text_splitter.separator)
 
       new_current_doc =
         acc.current_doc
         |> Enum.drop(1)
       merge_split_helper(
         d,
-        %{acc | total: new_total, current_doc: new_current_doc})
+        %{acc | total: new_total, current_doc: new_current_doc},
+        text_splitter)
     end
   end
 
-  defp merge_splits(splits) do
-    separator_len = String.length(@separator)
+  defp merge_splits(splits, %TextSplitter{} = text_splitter) do
+    separator_len = String.length(text_splitter.separator)
     first_split = List.first(splits)
     acc = %{current_doc: [first_split], docs: [], total: String.length(first_split)}
     output_acc = splits
@@ -47,19 +73,18 @@ defmodule LangChain.TextSplitter do
     |> Enum.reduce(
       acc,
       fn d, acc ->
-        IO.puts("REDUCTION!!!!!")
-        IO.inspect(acc)
         len = String.length(d)
         separator_length =
           if Enum.count(acc.current_doc) > 0,
-             do: String.length(@separator), else: 0
+             do: String.length(text_splitter.separator), else: 0
         
         acc =
-          if (acc.total + len + separator_length) > @chunk_size do
+          if (acc.total + len + separator_length) >
+               text_splitter.chunk_size do
                 if Enum.count(acc.current_doc) do
-                  doc = join_docs(acc.current_doc)
+                  doc = join_docs(acc.current_doc, text_splitter.separator)
                   acc = %{acc | docs: acc.docs ++ [doc]}
-                  merge_split_helper(d, acc)
+                  merge_split_helper(d, acc, text_splitter)
                 else
                   acc
                 end
@@ -72,6 +97,6 @@ defmodule LangChain.TextSplitter do
         }
       end
     )
-    output_acc.docs ++ [join_docs(output_acc.current_doc)]
+    output_acc.docs ++ [join_docs(output_acc.current_doc, text_splitter.separator)]
   end
 end
