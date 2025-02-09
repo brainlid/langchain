cccc70fd1964afc0dc3288868f83b680a5bf0e51
joaquin <iglesiastj@gmail.com>
2025-02-09 10:50:07 +0100

    add new test and fix assert order

--- test/text_splitter_test.exs
+++ test/text_splitter_test.exs
@@ -5,12 +5,19 @@ defmodule TextSplitterTest do
 
   describe "CharacterTextSplitter" do
     @tag :wip
-    test "text splitting by character count" do
+    test "Splitting by character count" do
       text = "foo bar baz 123"
       expected_output = ["foo bar", "bar baz", "baz 123"]
       output = TextSplitter.split_text(text)
-      assert output == expected_output
+      assert expected_output == output
+    end
+
+    @tag :wip
+    test "Splitting character by count doesn't create empty documents" do
+      text = "foo  bar"
+      expected_output = ["foo", "bar"]
+      output = TextSplitter.split_text(text)
+      assert expected_output == output
     end
   end
-  
 end
