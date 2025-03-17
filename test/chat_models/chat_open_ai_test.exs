ad26e7ff73248f9d4aca26cab2af9463b3543b32
Raul Pereira <raul.pereira@cloudwalk.io>
2025-03-17 12:05:47 -0300

    redact api-key in openai chat

--- test/chat_models/chat_open_ai_test.exs
+++ test/chat_models/chat_open_ai_test.exs
@@ -2112,4 +2112,15 @@ defmodule LangChain.ChatModels.ChatOpenAITest do
              }
     end
   end
+
+  describe "inspect" do
+    test "redacts the API key" do
+      chain = ChatOpenAI.new!()
+
+      changeset = Ecto.Changeset.cast(chain, %{api_key: "1234567890"}, [:api_key])
+
+      refute inspect(changeset) =~ "1234567890"
+      assert inspect(changeset) =~ "**redacted**"
+    end
+  end
 end
