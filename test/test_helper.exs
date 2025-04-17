a777179704b1d7df7ef6fe38cc6f61b1724968b2
Mase Graye <mase.graye@gmail.com>
2025-04-17 10:32:30 -0500

    Re-add env-based config

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,23 +1,23 @@
 # Load the ENV key for running live OpenAI tests.
-# Application.put_env(:langchain, :openai_key, System.fetch_env!("OPENAI_API_KEY"))
-# Application.put_env(:langchain, :anthropic_key, System.fetch_env!("ANTHROPIC_API_KEY"))
-# Application.put_env(:langchain, :google_ai_key, System.fetch_env!("GOOGLE_API_KEY"))
-# Application.put_env(:langchain, :aws_access_key_id, System.fetch_env!("AWS_ACCESS_KEY_ID"))
-# Application.put_env(:langchain, :perplexity_key, System.fetch_env!("PERPLEXITY_API_KEY"))
-# Application.put_env(:langchain, :mistral_api_key, System.fetch_env!("MISTRAL_API_KEY"))
+Application.put_env(:langchain, :openai_key, System.fetch_env!("OPENAI_API_KEY"))
+Application.put_env(:langchain, :anthropic_key, System.fetch_env!("ANTHROPIC_API_KEY"))
+Application.put_env(:langchain, :google_ai_key, System.fetch_env!("GOOGLE_API_KEY"))
+Application.put_env(:langchain, :aws_access_key_id, System.fetch_env!("AWS_ACCESS_KEY_ID"))
+Application.put_env(:langchain, :perplexity_key, System.fetch_env!("PERPLEXITY_API_KEY"))
+Application.put_env(:langchain, :mistral_api_key, System.fetch_env!("MISTRAL_API_KEY"))
 Application.put_env(:langchain, :groq_api_key, System.get_env("GROQ_API_KEY"))
 
-# Application.put_env(
-#   :langchain,
-#   :aws_secret_access_key,
-#   System.fetch_env!("AWS_SECRET_ACCESS_KEY")
-# )
+Application.put_env(
+  :langchain,
+  :aws_secret_access_key,
+  System.fetch_env!("AWS_SECRET_ACCESS_KEY")
+)
 
-# Application.put_env(
-#   :langchain,
-#   :aws_region,
-#   System.get_env("AWS_REGION", "us-east-1")
-# )
+Application.put_env(
+  :langchain,
+  :aws_region,
+  System.get_env("AWS_REGION", "us-east-1")
+)
 
 Mimic.copy(LangChain.Utils.BedrockStreamDecoder)
 Mimic.copy(LangChain.Utils.AwsEventstreamDecoder)
