5303d5b77a688d3215cede17413823d61f5aac97
Ellyse <ellyse@common.tools>
2025-05-01 21:17:05 -0700

    remove debugging

--- lib/message_processors/json_processor.ex
+++ lib/message_processors/json_processor.ex
@@ -154,7 +154,6 @@ defmodule LangChain.MessageProcessors.JsonProcessor do
           run(chain, %Message{message | processed_content: json})
 
         _ ->
-          IO.puts("failed to find json, message.process_content=#{message.processed_content}")
           {:halt, Message.new_user!("ERROR: No JSON found")}
       end
     end)
