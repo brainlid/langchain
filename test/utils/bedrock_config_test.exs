59eaf52019fb099ff372054dba6a10766b751165
Quang Nguyen <chox.negi@gmail.com>
2025-09-08 09:56:49 +0700

    Refactor BedrockConfig tests to also cover BedrockConfig.from_application_env!

--- test/utils/bedrock_config_test.exs
+++ test/utils/bedrock_config_test.exs
@@ -1,15 +1,21 @@
 defmodule LangChain.Utils.BedrockConfigTest do
+  alias Ecto.Changeset
   alias LangChain.Utils.BedrockConfig
   use ExUnit.Case, async: true
+  use Mimic
 
   test "supports aws credentials without session token" do
     Application
+    |> stub(:fetch_env!, fn app, key ->
+      case {app, key} do
+        {:langchain, :aws_access_key_id} -> "KEY"
+        {:langchain, :aws_secret_access_key} -> "SECRET"
+        {:langchain, :aws_region} -> "us-east-1"
+      end
+    end)
     |> stub(:fetch_env, fn :langchain, :aws_session_token -> :error end)
-    |> stub(:fetch_env!, fn :langchain, :aws_session_token -> :error end)
-    |> stub(:get_env, fn :langchain, :aws_region -> "us-east-1" end)
-    |> stub(:get_env, fn :langchain, :aws_session_token -> :error end)
 
-    bedrock_config = BedrockConfig.from_application_env!()
+    bedrock_config = bedrock_config()
 
     assert BedrockConfig.aws_sigv4_opts(bedrock_config) == [
              access_key_id: "KEY",
@@ -21,19 +27,29 @@ defmodule LangChain.Utils.BedrockConfigTest do
 
   test "supports aws credentials with session token" do
     Application
-    |> stub(:fetch_env, fn :langchain, :aws_session_token -> "TOKEN" end)
-    |> stub(:fetch_env!, fn :langchain, :aws_session_token -> "TOKEN" end)
-    |> stub(:get_env, fn :langchain, :aws_region -> "ap-southeast-2" end)
-    |> stub(:get_env, fn :langchain, :aws_session_token -> "TOKEN" end)
+    |> stub(:fetch_env!, fn app, key ->
+      case {app, key} do
+        {:langchain, :aws_access_key_id} -> "KEY"
+        {:langchain, :aws_secret_access_key} -> "SECRET"
+        {:langchain, :aws_region} -> "ap-southeast-2"
+      end
+    end)
+    |> stub(:fetch_env, fn :langchain, :aws_session_token -> {:ok, "TOKEN"} end)
 
-    bedrock_config = BedrockConfig.from_application_env!()
+    bedrock_config = bedrock_config()
 
     assert BedrockConfig.aws_sigv4_opts(bedrock_config) == [
+             token: "TOKEN",
              access_key_id: "KEY",
              secret_access_key: "SECRET",
-             token: "TOKEN",
              region: "ap-southeast-2",
              service: :bedrock
            ]
+  end
+
+  defp bedrock_config() do
+    %BedrockConfig{}
+    |> BedrockConfig.changeset(BedrockConfig.from_application_env!())
+    |> Changeset.apply_changes()
   end
 end
