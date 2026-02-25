9b4e9f433d146968d2baed153004e32937728ac1
Jinkyou Son <nallwhy@gmail.com>
2026-02-25 21:57:50 +0900

    Bump req >= 0.5.3

--- mix.exs
+++ mix.exs
@@ -45,7 +45,7 @@ defmodule LangChain.MixProject do
     [
       {:ecto, "~> 3.10 or ~> 3.11"},
       {:gettext, "~> 0.26.2 or ~> 1.0.0"},
-      {:req, ">= 0.5.2"},
+      {:req, ">= 0.5.3"},
       {:nimble_parsec, "~> 1.4", optional: true},
       {:abacus, "~> 2.1.0", optional: true},
       {:nx, ">= 0.7.0", optional: true},
